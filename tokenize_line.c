b20c504d5572c983171ae704f514591576f24c6b
David Tolza <9769@holbertonstudents.com>
2024-12-20 14:43:59 +0100

    tokenize

--- tokenize_line.c
+++ tokenize_line.c
@@ -1,18 +1,36 @@
-// Fonction pour extraire les tokens et les stocker dans un tableau dynamique
-char **tokenize_line(char *line, const char *delim, int token_count) {
-    char **tokens = malloc((token_count + 1) * sizeof(char *));
-    if (!tokens) {
-        perror("Error allocating memory for tokens");
-        return NULL;
-    }
+#include "main.h"
 
-    int i = 0;
-    char *token = strtok(line, delim);
-    while (token) {
-        tokens[i++] = token;
-        token = strtok(NULL, delim);
-    }
-    tokens[i] = NULL; // Terminer avec NULL
+/**
+ * tokenize_line - splits a line into tokens based on delimiters
+ *@line: the line to split
+ *@delim: the characters delimiting the tokens
+ *@token_count: the number of tokens expected
+ * Return: an array of pointers to the token, or NULL on error
+ */
 
-    return tokens;
+
+/* Fonction pour extraire les tokens et les stocker dans un tableau dynamique*/
+
+char **tokenize_line(char *line, const char *delim, int token_count)
+{
+	char **tokens = malloc((token_count + 1) * sizeof(char *));
+
+	if (!tokens)
+	{
+		perror("Error allocating memory for tokens");
+		return (NULL);
+	}
+
+	int i = 0;
+
+	char *token = strtok(line, delim);
+
+	while (token)
+	{
+		tokens[i++] = token;
+		token = strtok(NULL, delim);
+	}
+	tokens[i] = NULL;
+
+	return (tokens);
 }
