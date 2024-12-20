3ffa3a23f6f4d1ecd23c7354b7a7cad77d00e6f9
David Tolza <9769@holbertonstudents.com>
2024-12-20 14:43:10 +0100

    count tokens

--- count_tokens.c
+++ count_tokens.c
@@ -1,18 +1,33 @@
-// Fonction pour compter le nombre de tokens dans une ligne
-int count_tokens(const char *line, const char *delim) {
-    int count = 0;
-    char *line_copy = strdup(line);
-    if (!line_copy) {
-        perror("Error duplicating line");
-        return -1;
-    }
+#include "main.h"
 
-    char *token = strtok(line_copy, delim);
-    while (token) {
-        count++;
-        token = strtok(NULL, delim);
-    }
+/**
+ * count_tokens - Counts the number of tokens in a line
+ * @line: The line to split into tokens
+ * @delim: The characters delimiting the tokens
+ * Return: The number of tokens found, or -1 on error
+ */
 
-    free(line_copy);
-    return count;
+
+/* Fonction pour compter le nombre de tokens dans une ligne*/
+
+int count_tokens(const char *line, const char *delim)
+{
+	int count = 0;
+	char *line_copy = strdup(line);
+
+	if (!line_copy)
+	{
+		perror("Error duplicating line");
+		return (-1);
+	}
+
+	char *token = strtok(line_copy, delim);
+
+	while (token)
+	{
+		count++;
+		token = strtok(NULL, delim);
+	}
+	free(line_copy);
+	return (count);
 }
