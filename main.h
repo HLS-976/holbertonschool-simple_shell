af01403fc3f14bfe16ce782ea6e90425a3519f4d
David Tolza <9769@holbertonstudents.com>
2024-12-20 14:46:14 +0100

    main.h

--- main.h
+++ main.h
@@ -1,7 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /* Includes libraries */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /* Prototypes */
+
+char *read_line(const char *prompt);
+
+char **tokenize_line(char *line, const char *delim, int token_count);
+
+int count_tokens(const char *line, const char *delim) ;
+
 #endif
