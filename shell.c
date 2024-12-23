0e2502c80c78d18927442624450eb7c11de2abad
David Tolza <9769@holbertonstudents.com>
2024-12-23 15:34:02 +0100

    integration fct execution

--- shell.c
+++ shell.c
@@ -1,40 +1,55 @@
 #include "main.h"
 
-// Fonction principale
-int main(void) {
-    const char *prompt = ">>> ";
-    const char *delim = " \t\n"; // Espaces, tabulations et nouvelles lignes
-    char *lineptr;
+/**
+ * main - Shell program entry point
+ * Return: 0 si le programme s'exécute avec succès, -1 en cas d'erreur
+ */
 
-    while (1) {
-        lineptr = read_line(prompt);
-        if (!lineptr)
-            break;
+/*Fonction principale*/
 
-        int token_count = count_tokens(lineptr, delim);
-        if (token_count <= 0) {
-            free(lineptr);
-            continue;
-        }
+int main(void)
+{
+	const char *prompt = "(shell)-$";
+	const char *delim = " \t\n";
+	char *lineptr;
+	int token_count;
+	char *line_copy;
+	char **tokens;
 
-        char *line_copy = strdup(lineptr);
-        if (!line_copy) {
-            perror("Error duplicating line");
-            free(lineptr);
-            continue;
-        }
+	while (1)
+	{
+		lineptr = read_line(prompt);
+		if (!lineptr)
+			break;
+		token_count = count_tokens(lineptr, delim);
 
-        char **tokens = tokenize_line(line_copy, delim, token_count);
-        if (!tokens) {
-            free(lineptr);
-            free(line_copy);
-            continue;
-        }
+		if (token_count <= 0)
+		{
+			free(lineptr);
+			continue;
+		}
+		line_copy = strdup(lineptr);
 
-        free(tokens);
-        free(line_copy);
-        free(lineptr);
-    }
+		if (!line_copy)
+		{
+			perror("Error duplicating line");
+			free(lineptr);
+			continue;
+		}
+		tokens = tokenize_line(line_copy, delim, token_count);
 
-    return 0;
+		if (!tokens)
+		{
+			free(lineptr);
+			free(line_copy);
+			continue;
+		}
+		/*app la fonction execution*/
+		execution(tokens, token_count);
+
+		free(tokens);
+		free(line_copy);
+		free(lineptr);
+	}
+	return (0);
 }
