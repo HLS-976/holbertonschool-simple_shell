2b19cbfc4f005ba5a3e6c8e59e8e72d8e5fce6a8
David Tolza <9769@holbertonstudents.com>
2024-12-30 11:26:44 +0100

    modification prompt

--- read_line.c
+++ read_line.c
@@ -2,7 +2,6 @@
 
 /**
  * read_line - Reads a line from the user with a prompt
- * @prompt: The prompt to display to prompt the user for a line
  * Return: A pointer to the line read, or NULL on error
  */
 
@@ -13,8 +12,6 @@ char *read_line(const char *prompt)
 	char *lineptr = NULL;
 	size_t length = 0;
 	ssize_t chars_read;
-
-	printf("%s", prompt);
 
 	chars_read = getline(&lineptr, &length, stdin);
 
