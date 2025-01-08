d94e61df79a0fc21e151577712b8129030745285
hls <hilliassmirali@gmail.com>
2025-01-08 03:28:48 +0100

    remove prototype of child_exit function

--- main.h
+++ main.h
@@ -20,8 +20,6 @@ int execution(char *argv[]);
 
 int is_interactive(void);
 
-int child_exit(char **tokens, char *line_copy, ssize_t nread);
-
 char *find_command_in_path(char *command);
 
 int is_interactive(void);
